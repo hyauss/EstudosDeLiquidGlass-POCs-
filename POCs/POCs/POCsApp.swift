3c0b66de527c361ad7fe797bed10114227a6dfa8
Vinicius A. Marques <vini.amarques10@gmail.com>
2025-10-20 16:26:37 -0300

    feat: enviroment

---
+++ POCs/POCs/POCsApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCsApp.swift
+//  POCs
+//
+//  Created by Vinicius Alves Marques on 20/10/25.
+//
+
+import SwiftUI
+
+@main
+struct POCsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ POCs/POCsTests/POCsTests.swift
@@ -0,0 +1,17 @@
+//
+//  POCsTests.swift
+//  POCsTests
+//
+//  Created by Vinicius Alves Marques on 20/10/25.
+//
+
+import Testing
+@testable import POCs
+
+struct POCsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
